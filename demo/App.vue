ca7c46e252d91f24dd56f38c3e8c4c18fae07f43
2282318893 <2282318893@qq.com>
2024-05-13 10:33:01 +0800

    test

---
+++ demo/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="wrapper">
+		<h1 class="title">💐 Vue3 + Tiptap 🚀</h1>
+		<x-tiptap />
+	</div>
+</template>
+<script setup></script>
+<style scoped lang="scss">
+.wrapper {
+	.title {
+		text-align: center;
+		padding: 20px 0;
+		font-weight: 600;
+    font-size: 22px;
+		color: #72aaab;
+	}
+	margin: 0 auto;
+	width: 80%;
+	padding: 60px 20px;
+}
+</style>
